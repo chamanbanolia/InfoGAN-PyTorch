21db61167e5b60d15dd9bb3308984a79662ca5d4
chamanbanolia <44977122+chamanbanolia@users.noreply.github.com>
2024-04-15 10:24:31 +0530

    Update train.py to check type of input to criterionD

--- train.py
+++ train.py
@@ -146,6 +146,7 @@ for epoch in range(params['num_epochs']):
         label = torch.full((b_size, ), real_label, device=device)
         output1 = discriminator(real_data)
         probs_real = netD(output1).view(-1)
+        print(type(probs_real), type(label))
         loss_real = criterionD(probs_real, label)
         # Calculate gradients.
         loss_real.backward()
