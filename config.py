d29166760a7b5b83fc74d4d1424eba1f753a05d1
chamanbanolia <44977122+chamanbanolia@users.noreply.github.com>
2024-04-20 11:06:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ params = {
     'learning_rate': 2e-4,# Learning rate.
     'beta1': 0.5,
     'beta2': 0.999,
-    'save_epoch' : 1,# After how many epochs to save checkpoints and generate test output.
+    'save_epoch' : 20,# After how many epochs to save checkpoints and generate test output.
     'dataset' : 'PatternNet'}# Dataset to use. Choose from {MNIST, SVHN, CelebA, FashionMNIST, PatternNet}. CASE MUST MATCH EXACTLY!!!!!
