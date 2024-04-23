4738631d9344b2e7b3e189ded9ab224694ef77b9
chamanbanolia <44977122+chamanbanolia@users.noreply.github.com>
2024-04-23 12:37:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # Dictionary storing network parameters.
 params = {
     'batch_size': 64,# Batch size.
-    'num_epochs': 100,# Number of epochs to train for.
+    'num_epochs': 50,# Number of epochs to train for.
     'learning_rate': 2e-4,# Learning rate.
     'beta1': 0.5,
     'beta2': 0.999,
-    'save_epoch' : 20,# After how many epochs to save checkpoints and generate test output.
+    'save_epoch' : 10,# After how many epochs to save checkpoints and generate test output.
     'dataset' : 'PatternNet'}# Dataset to use. Choose from {MNIST, SVHN, CelebA, FashionMNIST, PatternNet}. CASE MUST MATCH EXACTLY!!!!!
